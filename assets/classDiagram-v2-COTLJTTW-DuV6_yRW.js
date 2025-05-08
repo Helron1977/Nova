79a19675387afcf106471e1a26e0180ee1d715e6
Helron1977 <47913777+Helron1977@users.noreply.github.com>
2025-05-08 15:27:54 +0200

    Updates

---
+++ assets/classDiagram-v2-COTLJTTW-DuV6_yRW.js
@@ -0,0 +1,1 @@
+import{s as a,c as s,a as e,C as t}from"./chunk-A2AXSNBT-BYJ1h_Ei.js";import{_ as i}from"./vendor_mermaid_deps-DFgiPUWA.js";import"./index-Cqx1Fed1.js";import"./chunk-RZ5BOZE2-C6gCwhLH.js";var _={parser:e,get db(){return new t},renderer:s,styles:a,init:i(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{_ as diagram};
