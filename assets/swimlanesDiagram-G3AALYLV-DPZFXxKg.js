30ce0f094e38cef180be7cdb1e14ed6359c0f4dd
Helron1977 <47913777+Helron1977@users.noreply.github.com>
2026-07-23 21:18:56 +0200

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-DPZFXxKg.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-D6HfOMWr.js";import{_ as a}from"./index-DzXTuQHC.js";import"./chunk-5VM5RSS4-DfkPe_pI.js";import"./chunk-XXDRQBXY-D5ntlJPy.js";import"./chunk-VR4S4FIN-DWCWMwoU.js";import"./chunk-32BRIVSS-zcl4l8P4.js";import"./channel-QgHkJxy0.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
