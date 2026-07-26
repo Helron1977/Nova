fd3ba6b069b43517723519404e1947a80cb3b491
Helron1977 <47913777+Helron1977@users.noreply.github.com>
2026-07-26 17:23:49 +0200

    Updates

--- assets/swimlanesDiagram-G3AALYLV-D96wuUoQ.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U-BPWQL7st.js";import{_ as a}from"./index-oyGjOG9v.js";import"./chunk-5VM5RSS4-BxHMWcGd.js";import"./chunk-XXDRQBXY-CY7mhWGr.js";import"./chunk-VR4S4FIN-Lug8XhEi.js";import"./chunk-32BRIVSS-1I0AOh_L.js";import"./channel-DDjmHUYw.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
---
+++ assets/swimlanesDiagram-G3AALYLV-PdmSduBB.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-Cds6pyxP.js";import{_ as a}from"./index-CDWv46m4.js";import"./chunk-5VM5RSS4-BDjWxjB-.js";import"./chunk-XXDRQBXY-CYlp2d6l.js";import"./chunk-VR4S4FIN-ZB0C6ihy.js";import"./chunk-32BRIVSS-C65H4INl.js";import"./channel-BEQm9nPf.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
